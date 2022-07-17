1625b3c0e58a7916ae5f22851be5e96742d87794
chertoha <a.chertok@akatech.com.ua>
2022-07-17 06:25:43 +0000

    Deploying to gh-pages from @ chertoha/ice-cream-project-group-28@92d7b47801ed56351185c75658fa7c4327bb30c3 🚀

--- index.e0d41aec.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".mob-menu__open-btn"),closeMenuBtn:document.querySelector(".mob-menu__close-btn"),menu:document.querySelector(".backdrop-mob"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.e0d41aec.js.map
